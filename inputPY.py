8d5106b3170b77296886ec00811ade81220b8551
Yasin Beken <yasinbeken16@gmail.com>
2024-03-03 15:13:15 +0300

    First Commit

---
+++ inputPY.py
@@ -0,0 +1,10 @@
+# name=input("Please write something: ")
+# print(f"Name: {name} Type: {type(name)}")
+
+number="123"
+number=int(number)
+print(type(number))
+number=float(number)
+print(type(number))
+number=str(number)
+print(type(number))
